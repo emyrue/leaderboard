2ffb3874b6ef94f5b147189e2014b5a7eb5438b2
emyrue <94935633+emyrue@users.noreply.github.com>
2022-04-06 15:54:57 -0600

    Add scores function working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 import createGame, { getID } from './modules/createGame.js';
+import addScore from './modules/addScore.js';
+import Scores from './modules/scores.js';
 
 const newGame = {
   name: 'throw the chalk at jimmy',
@@ -10,3 +12,7 @@ const game = JSON.stringify(newGame);
 if (!getID()) {
   await createGame(game);
 }
+
+const newScore = JSON.stringify(new Scores('Emily', 350));
+
+addScore(newScore);
--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,6 +1,16 @@
-import { getID } from './createGame.js';
-
 const key = 'zRNV29Vwiw2Q5Vnvo5gE'
 
 const scoreURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/`;
 
+const addScore = async (object) => {
+  const newScore = await fetch(scoreURL, {
+    method: "POST",
+    body: object,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  newScore.json().then(result => console.log(result.result));
+};
+
+export default addScore;
