c1c9c79e875dc15361fcd09d71a22fd2940b6053
emyrue <94935633+emyrue@users.noreply.github.com>
2022-04-06 09:49:08 -0600

    successfully used fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+const gameURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const getGame = new Request(gameURL);
+const gettingGame = await fetch(getGame);
+const text = await gettingGame.text();
+
+const newGame = {
+  name: 'throw the chalk at jimmy',
+};
+
+const game = JSON.stringify(newGame);
+
+const test = document.createElement('div');
+test.textContent = text;
+document.body.appendChild(test);
--- webpack.config.js
+++ webpack.config.js
@@ -40,4 +40,7 @@ module.exports = {
       },
     ],
   },
+  experiments: {
+    topLevelAwait: true,
+  },
 };
